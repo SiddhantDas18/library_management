aaa62f9980599317f05b4878f37da52849e1a8b9
siddhantdas18 <siddhantdas18@gmail.com>
2025-05-07 14:22:57 +0530

    Added signin with token generation

---
+++ app/api/signin/route.ts
@@ -0,0 +1,55 @@
+import { NextRequest,NextResponse } from "next/server";
+import prismaClient from "@/app/lib/db";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const secret = process.env.SECRET as string
+
+export async function POST(req:NextRequest) {
+    
+    const request = await req.json()
+
+    const {username,password} = await request
+
+    try{
+
+        const findUser = await prismaClient.users.findFirst({
+            where:{
+                username:username
+            }
+        })
+    
+        if(!findUser){
+            return NextResponse.json({
+                msg:"User not found"
+            })
+        }
+
+        const hashed_password = findUser?.password
+        const checkpass = await bcrypt.compare(password,hashed_password)
+
+        if(!checkpass){
+            return NextResponse.json({
+                msg:"Password not matched"
+            })
+        }
+
+        const id= findUser?.id
+
+        const token = jwt.sign({
+            id
+        },secret)
+
+        return NextResponse.json({
+            msg:"SignedUp successful",
+            token:token
+        })
+
+    }catch(e){
+        return NextResponse.json({
+            msg:(e as Error).toString()
+        })
+    }
+
+    
+}
